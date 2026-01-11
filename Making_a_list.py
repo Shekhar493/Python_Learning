6fcf2f3f38b40d0e71edeac159f156341ef35366
Chandra Shekhar Yadav <chandrashekhary866@gmail.com>
2026-01-11 14:48:05 +0545

    Add files via upload

---
+++ Making_a_list.py
@@ -0,0 +1,12 @@
+Movies = []
+
+mov1 = input("Enter your first favorite movie: ")
+mov2 = input("Enter your second favorite movie: ")
+mov3 = input("Enter your third favorite movie: ")
+
+Movies.append(mov1)
+Movies.append(mov2)
+Movies.append(mov3)
+
+print("Your favorite movies are:", Movies)
+
