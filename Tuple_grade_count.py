68408478799e69c472cb16cf638e31bdc4b4d1f5
Chandra Shekhar Yadav <chandrashekhary866@gmail.com>
2026-01-11 15:24:36 +0545

    Add files via upload

---
+++ Tuple_grade_count.py
@@ -0,0 +1,11 @@
+grade = ("C", "D", "A", "A", "B", "B","A")
+grade_count = grade.count("A")
+print("The count of grade A is:", grade_count)
+
+
+grade_list = list(grade)
+print("The list version of tuple is:", grade_list)
+grade_list.sort()
+print("The sorted list is:", grade_list)
+
+
